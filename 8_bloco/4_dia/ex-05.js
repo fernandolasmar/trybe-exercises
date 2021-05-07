5507f44a93d004c7b51ed559536bc4381978cd27
fernando lasmar <f3rnandolasmar@gmail.com>
2021-05-06 21:43:37 -0300

    ex 5

--- 8_bloco/4_dia/ex-05.js
+++ 8_bloco/4_dia/ex-05.js
@@ -11,7 +11,18 @@ const names = [
 
 function containsA() {
   // escreva seu código aqui
-  
+  return names.reduce((acc, curr) => 
+    acc + curr.split('').reduce((acc2, curr2) => {
+      if (curr2 === 'a' || curr2 === 'A')
+      return acc2 + 1;
+      // console.log(acc2 + 1);
+      // console.log(acc2);
+      return acc2;
+    }, 0),
+  0);
 }
+console.log(containsA(names));
 
 assert.deepStrictEqual(containsA(), 20);
+
+// O método split () é usado para dividir uma string em uma matriz de substrings e retorna a nova matriz.
