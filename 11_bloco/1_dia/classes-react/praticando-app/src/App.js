d644d8a719bc80e0e88f36c0af8317239bdb2dcb
fernando lasmar <f3rnandolasmar@gmail.com>
2021-05-24 20:10:43 -0300

    praticando

---
+++ 11_bloco/1_dia/classes-react/praticando-app/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Meu nome é: Fernando Lasmar</h1>
+      <p>Sou de Belo Horizonte, sou casado e tenho um filho, meu hobby é jogar games no PC e passeios de moto.</p>        
+    </div>
+  );
+}
+
+export default App;
