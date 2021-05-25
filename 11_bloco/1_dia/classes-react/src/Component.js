3131d520e13936ad55c2a6febf4e5c374df1adb2
fernando lasmar <f3rnandolasmar@gmail.com>
2021-05-25 19:12:22 -0300

    praticando react

---
+++ 11_bloco/1_dia/classes-react/src/Component.js
@@ -0,0 +1,12 @@
+
+function Component() {
+  return (
+    <div>
+      <h1>Meu nome é Fernando Lasmar</h1>
+
+      <p>Sou "Brasileiro" nascido em "Belo Horizonte", casado e tenho um filho de 4 anos de idade, meu hobby é jogar games no pc, passeio de moto e bike.</p>
+
+    </div>
+  );
+}
+export default Component;
