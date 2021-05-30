d8f447e57222419b6b22b8535fdfc06a0246f480
fernando lasmar <f3rnandolasmar@gmail.com>
2021-05-30 19:48:55 -0300

    componente dados pessoais

---
+++ 12_bloco/2_dia/my-form-2.0/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import DadosPessoaisForm from './DadosPessoais';
+import './App.css';
+
+
+class App extends Component {
+  render() {
+    return (
+      <DadosPessoaisForm />
+    )
+      
+  }
+}
+
+export default App;
---
+++ 12_bloco/2_dia/my-form-2.0/src/DadosPessoais.jsx
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+
+const cidades = ['Acre', 'Alagoas', 'Amapá', 'Amazonas', 'Bahia', 'Ceará', 'Distrito Federal', 'Espírito Santo', 'Goiás', 'Maranhão', 'Mato Grosso', 'Mato Grosso do Sul', 'Minas Gerais', 'Pará', 'Paraíba', 'Paraná', 'Pernambuco', 'Piauí', 'Rio de Janeiro', 'Rio Grande do Norte', 'Rio Grande do Sul', 'Rondônia', 'Roraima', 'Santa Catarina', 'São Paulo', 'Sergipe', 'Tocantins' ];
+
+
+class DadosPessoaisForm extends Component {
+  render() {
+    return (
+      <fieldset>
+        <legend>Dados Pessoais</legend>
+        <div>
+          <label>
+            Nome:<br/> 
+            <input type="name" name="name" maxLength="40" required/>
+          </label>
+        </div>
+        <div>
+          <label>
+            Email:<br/>  
+            <input type="name" name="email" maxLength="50" required />
+          </label>
+        </div>
+        <div>
+          <label>
+            CPF:<br/>  
+            <input type="name" name="cpf" maxLength="11" required />
+          </label>
+        </div>
+        <div>
+          <label>
+            Endereço:<br/>  
+            <input type="name" name="endereço" maxLength="200" required/>
+          </label>
+        </div>
+        <div>
+          <label>
+            Cidade:<br/>  
+            <input type="name" name="cidade" maxLength="28" required/>
+          </label>
+        </div>
+        <div>
+          Estado:<br/>  
+          <select type="name" name="estado" maxLength="40" required>
+            <option>Selecione</option>
+          </select>  
+        </div>
+        <div>
+          <label>
+            Casa: 
+            <input type="radio" name="casa" required/>
+          </label>
+        </div>
+        <div>
+          <label>
+            Apartamento: 
+            <input type="radio" name="apartamento" required/>
+          </label>
+        </div>
+      </fieldset>
+    
+    )
+  }
+}
+
+export default DadosPessoaisForm;
