c6d620e82fc0996b57e0abbc9ee3eb3baf70b182
fernando lasmar <f3rnandolasmar@gmail.com>
2021-05-30 21:21:37 -0300

    adicionando componentes e css

--- 12_bloco/2_dia/my-form-2.0/src/App.js
+++ 12_bloco/2_dia/my-form-2.0/src/App.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 import DadosPessoaisForm from './DadosPessoais';
+import DadosProfissional from './DadosProfissional';
+import FormBtn from './FormBtn';
 import './App.css';
 
 
 class App extends Component {
   render() {
     return (
-      <DadosPessoaisForm />
+      <main className="main-app">
+        <DadosPessoaisForm />
+        <DadosProfissional />
+        <FormBtn />
+      </main>
     )
       
   }
--- 12_bloco/2_dia/my-form-2.0/src/DadosPessoais.jsx
+++ 12_bloco/2_dia/my-form-2.0/src/DadosPessoais.jsx
@@ -6,57 +6,59 @@ const cidades = ['Acre', 'Alagoas', 'Amapá', 'Amazonas', 'Bahia', 'Ceará', 'Di
 class DadosPessoaisForm extends Component {
   render() {
     return (
-      <fieldset>
-        <legend>Dados Pessoais</legend>
-        <div>
-          <label>
-            Nome:<br/> 
-            <input type="name" name="name" maxLength="40" required/>
-          </label>
-        </div>
-        <div>
-          <label>
-            Email:<br/>  
-            <input type="name" name="email" maxLength="50" required />
-          </label>
-        </div>
-        <div>
-          <label>
-            CPF:<br/>  
-            <input type="name" name="cpf" maxLength="11" required />
-          </label>
-        </div>
-        <div>
-          <label>
-            Endereço:<br/>  
-            <input type="name" name="endereço" maxLength="200" required/>
-          </label>
-        </div>
-        <div>
-          <label>
-            Cidade:<br/>  
-            <input type="name" name="cidade" maxLength="28" required/>
-          </label>
-        </div>
-        <div>
-          Estado:<br/>  
-          <select type="name" name="estado" maxLength="40" required>
-            <option>Selecione</option>
-          </select>  
-        </div>
-        <div>
-          <label>
-            Casa: 
-            <input type="radio" name="casa" required/>
-          </label>
-        </div>
-        <div>
-          <label>
-            Apartamento: 
-            <input type="radio" name="apartamento" required/>
-          </label>
-        </div>
-      </fieldset>
+      <div>
+        <h1 className="h1-formulario">Formulário</h1>
+        <fieldset>
+          <legend>Dados Pessoais</legend>
+          <div>
+            <label>
+              Nome:<br/> 
+              <input type="name" name="name" maxLength="40" required/>
+            </label>
+          </div>
+          <div>
+            <label>
+              Email:<br/>  
+              <input type="name" name="email" maxLength="50" required />
+            </label>
+          </div>
+          <div>
+            <label>
+              CPF:<br/>  
+              <input type="name" name="cpf" maxLength="11" required />
+            </label>
+          </div>
+          <div>
+            <label>
+              Endereço:<br/>  
+              <input type="name" name="endereço" maxLength="200" required/>
+            </label>
+          </div>
+          <div>
+            <label>
+              Cidade:<br/>  
+              <input type="name" name="cidade" maxLength="28" required/>
+            </label>
+          </div>
+          <div>
+            Estado:<br/>  
+            <select type="name" name="estado" required>
+              <option>Selecione</option>
+            </select>  
+          </div>
+          <div>
+            <label>
+              Casa: 
+              <input type="radio" name="casa" required/>
+            </label>
+            <label>
+              Apartamento: 
+              <input type="radio" name="apartamento" required/>
+            </label>
+          </div>
+        
+        </fieldset>
+      </div>
     
     )
   }
