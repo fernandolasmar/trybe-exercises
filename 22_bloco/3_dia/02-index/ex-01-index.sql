72f038cb27e8818ffcdb55811de78d23e2ecc847
fernando lasmar <f3rnandolasmar@gmail.com>
2021-09-02 23:01:45 -0300

    exercicio 1 index

---
+++ 22_bloco/3_dia/02-index/ex-01-index.sql
@@ -0,0 +1,11 @@
+/*
+Verifique o impacto de um FULLTEXT INDEX na tabela category (banco de dados sakila ), adicionando-o na coluna name . 
+Após ter adicionado o índice, mensure o custo da query utilizando o execution plan , como já foi feito em lições anteriores. 
+Após ter criado e mensurado o custo da query, exclua o índice e mensure novamente esse custo.
+*/
+
+CREATE FULLTEXT INDEX index_name ON sakila.category(name);
+
+select * from sakila.category where MATCH(name) against('Sports');
+
+DROP INDEX index_name ON sakila.category;
