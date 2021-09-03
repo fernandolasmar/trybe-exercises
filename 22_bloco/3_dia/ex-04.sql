e5428036e6021356aac9bb00a7b0e3e0d97bd94a
fernando lasmar <f3rnandolasmar@gmail.com>
2021-09-02 22:43:28 -0300

    exercicio 4

---
+++ 22_bloco/3_dia/ex-04.sql
@@ -0,0 +1,13 @@
+/*
+Crie uma view chamada movies_languages , usando as tabelas film e language do banco de dados sakila .
+Sua view deve exibir o título do filme , o id do idioma e o idioma do filme , como na imagem a seguir.
+*/
+
+CREATE VIEW movies_languages AS
+SELECT f.title,
+	l.language_id,
+	l.name AS language
+FROM sakila.film f
+INNER JOIN sakila.language l ON l.language_id = f.language_id;
+
+SELECT * FROM movies_languages;
