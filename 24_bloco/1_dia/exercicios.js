f4d3624f6c20914c5fa18ed24947f19c38959bfb
fernando lasmar <f3rnandolasmar@gmail.com>
2021-09-13 20:51:28 -0300

    Adicionando ex11

--- 24_bloco/1_dia/exercicios.js
+++ 24_bloco/1_dia/exercicios.js
@@ -62,5 +62,10 @@ db.movies.updateOne(
 );
 
 // Exercício 11 : Utilizando uma única operação, crie um campo chamado sequels e atribua a ele o valor 0 em todos os documentos.
+db.movies.updateMany(
+  {},
+  { $set: { sequels: 0 } }
+);
+
 // Exercício 12 : Utilizando uma única operação, remova os campos budget e estimatedBudget em todos os documentos.
 // Exercício 13 : Para os filmes Batman ou Home Alone , atribua a imdbRating o valor 17 , caso o valor de imdbRating seja menor que 17 .
