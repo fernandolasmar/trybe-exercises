b33e6f831df6086bc2895fcee0f14f86fb9ff2fb
fernando lasmar <f3rnandolasmar@gmail.com>
2021-09-13 20:17:38 -0300

    Adicionando ex03

--- 24_bloco/1_dia/exercicios.js
+++ 24_bloco/1_dia/exercicios.js
@@ -1,16 +1,21 @@
 // Exercício 1 : Altere o imdbRating para 7.7 no filme Batman .
 db.movies.updateOne(
   { title: "Batman"},
-  { $set: { imdbRating: "7.7" } }
+  { $set: { imdbRating: 7.7 } }
 );
 
 // Exercício 2 : Altere budget para 1 no filme Godzilla .
 db.movies.updateOne(
   { title: "Godzilla"},
-  { $set: { "budget": "1" } }
+  { $set: { "budget": 1 } }
 );
 
 // Exercício 3 : Altere budget para 15 e imdbRating para 5.5 no filme Home Alone .
+db.movies.updateOne(
+  { title: "Home Alone" },
+  { $set: { budget: 15, imdbRating: 5.5 } }
+);
+
 // Exercício 4 : Aumente em 2 o imdbRating do filme Batman .
 // Exercício 5 : Aumente em 5 o budget do filme Home Alone .
 // Exercício 6 : Multiplique por 4 o imdbRating do filme Batman .
