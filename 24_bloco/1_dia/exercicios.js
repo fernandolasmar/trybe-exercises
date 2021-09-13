e6542cd6272e8f0cd353d23d6b7e16f207a0304b
fernando lasmar <f3rnandolasmar@gmail.com>
2021-09-13 20:20:32 -0300

    Adicionando ex04

--- 24_bloco/1_dia/exercicios.js
+++ 24_bloco/1_dia/exercicios.js
@@ -17,6 +17,11 @@ db.movies.updateOne(
 );
 
 // Exercício 4 : Aumente em 2 o imdbRating do filme Batman .
+db.movies.update(
+  { title: "Batman"},
+  { $inc: { imdbRating: 2 } }
+);
+
 // Exercício 5 : Aumente em 5 o budget do filme Home Alone .
 // Exercício 6 : Multiplique por 4 o imdbRating do filme Batman .
 // Exercício 7 : Renomeie o campo budget para estimatedBudget do filme Batman .
