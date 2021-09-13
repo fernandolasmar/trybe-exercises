d9b6ebc2a4221e398dcf2c60635f8e50ada07c02
fernando lasmar <f3rnandolasmar@gmail.com>
2021-09-13 20:25:48 -0300

    Adicionando ex06

--- 24_bloco/1_dia/exercicios.js
+++ 24_bloco/1_dia/exercicios.js
@@ -29,6 +29,11 @@ db.movies.update(
 );
 
 // Exercício 6 : Multiplique por 4 o imdbRating do filme Batman .
+db.movies.update(
+  { title: "Batman"},
+  { $mul: { imdbRating: 4 } }
+);
+
 // Exercício 7 : Renomeie o campo budget para estimatedBudget do filme Batman .
 // Exercício 8 : Utilize o operador $min para alterar o budget para 5 do filme Home Alone .
 // Exercício 9 : Utilize o operador $max para alterar o imdbRating para 8.6 do filme Godzilla . Além disso, altere a categoria "adventure" para "thriller" do filme Godzilla .
