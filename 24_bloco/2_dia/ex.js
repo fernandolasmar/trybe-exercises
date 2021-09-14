52aceafe1770cad4a148909ed1f2bcb8bf90fe23
fernando lasmar <f3rnandolasmar@gmail.com>
2021-09-14 20:18:52 -0300

    Adicionando ex 11

--- 24_bloco/2_dia/ex.js
+++ 24_bloco/2_dia/ex.js
@@ -121,3 +121,36 @@ db.movies.updateOne(
   },
   { upsert: true }
 );
+
+// Exercício 11: Produza três querys para o filme Batman :
+
+// 01-Adicione o campo actor , que deve ser um array com o valor Christian Bale , ao array de cast em que o campo character seja igual a Batman ;
+db.movies.updateOne(
+  { 
+    title: "Batman",
+    "cast.character": "Batman"
+  },
+  {
+    $push: { "cast.$.actor": "Christian Bale" }
+  }  
+);
+// 02-Adicione o campo actor , que deve ser um array com o valor Michael Caine , ao array de cast em que o campo character seja igual a Alfred ;
+db.movies.updateOne(
+  { 
+    title: "Batman",
+    "cast.character": "Alfred"
+  },
+  {
+    $push: { "cast.$.actor": "Michael Caine" }
+  }  
+);
+// 03-Adicione o campo actor , que deve ser um array com o valor Heath Ledger , ao array de cast em que o campo character seja igual a Coringa .
+db.movies.updateOne(
+  { 
+    title: "Batman",
+    "cast.character": "Coringa"
+  },
+  {
+    $push: { "cast.$.actor": "Heath Ledger" }
+  }  
+);
