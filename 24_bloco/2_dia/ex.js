65ce74b5eb7d3350f893f5ea90d7bd94c1a3c5ac
fernando lasmar <f3rnandolasmar@gmail.com>
2021-09-14 19:26:29 -0300

    Adicionando ex 07

--- 24_bloco/2_dia/ex.js
+++ 24_bloco/2_dia/ex.js
@@ -50,3 +50,15 @@ db.movies.updateOne(
     $addToSet: { "category": "action" }
   }
 );
+
+// Exercício 7: Adicione a categoria "90's" aos filmes Batman e Home Alone .
+db.movies.updateMany(
+  { title: 
+    { 
+      $in: ["Batman", "Home Alone"] 
+    }
+  },
+  {
+    $push: { category: "90's" }
+  }
+);
