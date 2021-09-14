fb5b6cfb0499d1a6105e9401a603f92c80af0705
fernando lasmar <f3rnandolasmar@gmail.com>
2021-09-14 20:06:19 -0300

    Adicionando ex 10

--- 24_bloco/2_dia/ex.js
+++ 24_bloco/2_dia/ex.js
@@ -98,3 +98,26 @@ db.movies.updateOne(
     $set: { "cast.$.character": "Marv" }
   }  
 );
+
+// Exercício 10: Crie um array de documentos chamado cast para o filme Batman com os seguintes dados:
+db.movies.updateOne(
+  { title: "Batman"},
+  {
+    $push: {
+      cast: {
+        $each: [
+          {
+            "character": "Batman"
+          },
+          {
+            "character": "Alfred"
+          },
+          {
+            "character": "Coringa"
+          }
+        ]
+      }
+    }
+  },
+  { upsert: true }
+);
