03cd2c1067fb2a20465e0066cdbec723104f48e0
fernando lasmar <f3rnandolasmar@gmail.com>
2021-09-15 20:52:26 -0300

    Adicionando ex 13

--- 24_bloco/3_dia/ex.js
+++ 24_bloco/3_dia/ex.js
@@ -101,3 +101,17 @@ db.movies.find(
     category: { $size: 2 }
   },  
 ).pretty();
+
+// 13-Retorne os filmes da categoria "sci-fi" ou que possua o ratings maior do que 199 , exibindo apenas os campos title , ratings e category .
+db.movies.find(
+  { 
+    $or: [
+    { category: { $all: ["sci-fi"] } },
+    { ratings: { $elemMatch: { $gt: 199 } } }
+    ]
+  },
+  { title: 1,
+    ratings: 1,
+    category: 1, _id: 0
+  }
+);
