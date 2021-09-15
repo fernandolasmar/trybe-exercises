1e3415110db4223172c188ea8db1811268770aba
fernando lasmar <f3rnandolasmar@gmail.com>
2021-09-15 19:34:03 -0300

    Adicionando ex 03

--- 24_bloco/3_dia/ex.js
+++ 24_bloco/3_dia/ex.js
@@ -7,3 +7,14 @@ db.movies.find(
 db.movies.find(
   { category: { $all: ["action"] }, imdbRating:  { $gt: 7 }}  
 ).pretty();
+
+// 03-Adicione um array chamado ratings ao filme Batman com os seguintes valores: [85, 100, 102, 105] . Dica: lembre-se do operador $each visto no dia anterior.
+db.movies.updateOne(
+  { title: "Batman"},
+  { $push: { 
+      ratings: {
+        $each: [85, 100, 102, 105]
+      }
+    }
+  }
+);
