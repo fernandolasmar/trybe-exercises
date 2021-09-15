7a0bb0454fef52a0e0c3463d23af0d4c3f7e741d
fernando lasmar <f3rnandolasmar@gmail.com>
2021-09-15 20:16:09 -0300

    Adicionando ex 06

--- 24_bloco/3_dia/ex.js
+++ 24_bloco/3_dia/ex.js
@@ -40,3 +40,13 @@ db.movies.updateOne(
     }
   }
 );
+
+// 06-Retorne todos os filmes com ratings maior do que 103 , exibindo apenas os campos title e ratings .
+db.movies.find(
+  { 
+    ratings: { 
+      $elemMatch:  { $gt: 103 }
+    }
+  },  
+  { title: 1, ratings: 1, _id: 0 }  
+).pretty();
