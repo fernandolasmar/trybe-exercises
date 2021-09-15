9e0b833a56aa7ab913c66a85001ee0f72f8a9dc9
fernando lasmar <f3rnandolasmar@gmail.com>
2021-09-15 20:45:26 -0300

    Adicionando ex 12

--- 24_bloco/3_dia/ex.js
+++ 24_bloco/3_dia/ex.js
@@ -93,3 +93,11 @@ db.movies.find(
   { ratings: { $size: 4 }},
   { title: 1, _id: 0}
 ).pretty();
+
+// 12-Busque os filmes em que o módulo 5 do campo budget seja 0 e que o array category tenha tamanho 2 .
+db.movies.find(
+  { 
+    budget: { $mod: [5,0] },
+    category: { $size: 2 }
+  },  
+).pretty();
