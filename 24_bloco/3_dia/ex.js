bc92a99e0b1bf1ba92060e85ca1fbb23f0f15b0e
fernando lasmar <f3rnandolasmar@gmail.com>
2021-09-15 20:20:34 -0300

    Adicionando ex 07

--- 24_bloco/3_dia/ex.js
+++ 24_bloco/3_dia/ex.js
@@ -50,3 +50,13 @@ db.movies.find(
   },  
   { title: 1, ratings: 1, _id: 0 }  
 ).pretty();
+
+// 07-Retorne todos os filmes com ratings entre 100 e 105 , exibindo apenas os campos title e ratings .
+db.movies.find(
+  { 
+    ratings: { 
+      $elemMatch:  { $gte: 100, $lte: 105 }
+    }
+  },  
+  { title: 1, ratings: 1, _id: 0 }  
+).pretty();
