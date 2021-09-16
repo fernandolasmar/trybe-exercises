dbf5da45f428f2a9acf03b2006a2df5e75b5699d
fernando lasmar <f3rnandolasmar@gmail.com>
2021-09-15 21:03:44 -0300

    Adicionando ex 14

--- 24_bloco/3_dia/ex.js
+++ 24_bloco/3_dia/ex.js
@@ -115,3 +115,14 @@ db.movies.find(
     category: 1, _id: 0
   }
 );
+
+// 14-Retorne os filmes em que o ratings possua tamanho 4 e que seja da category "adventure" ou "family" , mas que não tenha o imdbRating menor que 7.
+db.movies.find(
+  { 
+    $and: [
+      { ratings: { $size: 4 } },
+      { category: { $in: ["adventure", "family"] } }, 
+      { imdbRating: { $not: { $lt: 7 } } },
+    ]
+  }
+);
