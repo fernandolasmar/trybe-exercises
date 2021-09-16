f3277822ab9dfdd4fb3d2d329defb45a97c78621
fernando lasmar <f3rnandolasmar@gmail.com>
2021-09-15 21:18:06 -0300

    Adicionando ex 18

--- 24_bloco/3_dia/ex.js
+++ 24_bloco/3_dia/ex.js
@@ -144,3 +144,8 @@ db.movies.updateOne(
   { title: "Home Alone" },
   { $set: { description: "An eight-year-old troublemaker must protect his house from a pair of burglars when he is accidentally left home alone by his family during Christmas vacation." } }
 );
+
+// 18-Utilizando o operador $regex , retorne todos os filmes em que a descrição comece com a palavra "The" .
+db.movies.find(
+  { description: { $regex: /^The/ } }
+).pretty();
