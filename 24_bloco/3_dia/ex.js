eb81aa4e71acf352a1928fdb82498b1cf3ff821f
fernando lasmar <f3rnandolasmar@gmail.com>
2021-09-15 21:51:49 -0300

    Adicionando ex 23

--- 24_bloco/3_dia/ex.js
+++ 24_bloco/3_dia/ex.js
@@ -169,3 +169,6 @@ db.movies.find(
 ).pretty();
 
 // 23-Utilizando o operador $text , busque por filmes que contenham a frase "when he is accidentally" .
+db.movies.find(
+  { $text: { $search: "\"when he is accidentally\"" } }
+).pretty();
