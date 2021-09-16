028e10fc0c01819e0ace5c7d6074dc70e595ece8
fernando lasmar <f3rnandolasmar@gmail.com>
2021-09-15 21:20:27 -0300

    Adicionando ex 19

--- 24_bloco/3_dia/ex.js
+++ 24_bloco/3_dia/ex.js
@@ -149,3 +149,8 @@ db.movies.updateOne(
 db.movies.find(
   { description: { $regex: /^The/ } }
 ).pretty();
+
+// 19-Utilizando o operador $regex , retorne todos os filmes em que a descrição termine com a palavra "humanity." .
+db.movies.find(
+  { description: { $regex: /humanity.$/ } }
+).pretty();
