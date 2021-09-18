66ce3a43fd195674161dc49c61e6c964a3c44d68
fernando lasmar <f3rnandolasmar@gmail.com>
2021-09-17 21:48:35 -0300

    Adicionando ex03 para fixar

--- 25_bloco/1_dia/para-fixar/03-para-fixar.js
+++ 25_bloco/1_dia/para-fixar/03-para-fixar.js
@@ -1,3 +1,35 @@
 // 01-Selecione todos os clientes com as suas respectivas transações feitas;
+db.clients.aggregate([
+  {
+    $lookup: {
+      from: "transactions",
+      localField: "name",
+      foreignField: "from",
+      as: "transactions_history"
+    },
+  },
+])
 // 02-Selecione quatro clientes com as suas respectivas transações recebidas;
+db.clients.aggregate([
+  {
+    $lookup: {
+      from: "transactions",
+      localField: "name",
+      foreignField: "to",
+      as: "transactions_history"
+    }
+  },
+  { $limit: 4 }
+]);
 // 03-Selecione todos os cliente do estado da "Florida" e suas respectivas transações recebidas
+db.clients.aggregate([
+  { $match: { State: "Florida" } },
+  {
+    $lookup: {
+      from: "transactions",
+      localField: "name",
+      foreignField: "to",
+      as: "transactions_history"
+    }
+  }
+]);
