3748d62733e3e6d435c7ec87b9da47e6004a077d
fernando lasmar <f3rnandolasmar@gmail.com>
2021-10-04 12:58:42 -0300

    Adicionando hello-express, recipes

---
+++ 26_bloco/4_dia/recipes/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+
+const app = express();
+
+const recipes = [
+  {id: 1, name: 'lasanha', price: 40.0, waitTime: 30},
+  { id: 2, name: 'Macarrão a Bolonhesa', price: 35.0, waitTime: 25 },
+  { id: 3, name: 'Macarrão com molho branco', price: 35.0, waitTime: 25 },
+];
+
+const drinks = [
+  { id: 1, name: 'Refrigerante Lata', price: 5.0 },
+  { id: 2, name: 'Refrigerante 600ml', price: 8.0 },
+  { id: 3, name: 'Suco 300ml', price: 4.0 },
+  { id: 4, name: 'Suco 1l', price: 10.0 },
+  { id: 5, name: 'Cerveja Lata', price: 4.5 },
+  { id: 6, name: 'Água Mineral 500 ml', price: 5.0 },
+];
+
+app.get('/recipes', (req, res) => {
+  res.json(recipes);
+});
+
+// Crie uma array drinks com os seguintes obejtos dentro dela e uma rota GET /drinks que retorna a lista de bebidas.
+app.get('/drinks', (req, res) => {
+  res.json(drinks);
+});
+
+app.listen(3000, () => {
+  console.log('Aplicação rodando na porta http://localhost:3000');
+});
