f10690555fdf4757f7261e261f18a08ba65a9662
fernando lasmar <f3rnandolasmar@gmail.com>
2021-11-30 22:07:21 -0300

    Add views new

--- 30_bloco/1_dia/mvc-example/index.js
+++ 30_bloco/1_dia/mvc-example/index.js
@@ -1,15 +1,22 @@
+const bodyParser = require('body-parser');
 const express = require('express');
 
 const AuthorController = require('./controllers/authorController');
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
 app.get('/authors', AuthorController.listAuthors);
 
+app.get('/authors/new', AuthorController.newAuthor);
+
 app.get('/authors/:id', AuthorController.showAuthor);
+
+app.get('/authors', AuthorController.createAuthor);
 
 const PORT = process.env.PORT || 3000;
 
